d3c6d920aa19ddd51e39e3198fa1e79fb2070636
omolo-j <omoloj135@gmail.com>
2024-04-27 15:15:41 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import react from 'react'
+import React from 'react'
+import TransactionList from './TransactionList';
+
+const fetchedTransactions = [
+  {date: "2019-12-01", description:"Paycheck from Bob's Burgers", category:"Income", amount: 1000 },
+  {date: "2019-12-01", description:"South by Southwest Quinoa Bowl at Fresh & Co", category:"Food", amount: -10.55 },
+  {date: "2019-12-02", description:"South by Southwest Quinoa Bowl at Fresh & Co", category:"Food", amount: -10.55 },
+  {date: "2019-12-04", description:"Sunglasses, Urban Outfitters", category:"Fashion", amount: -24.99 },
+  {date: "2019-12-06", description:"Venmo, Alice Pays you for Burrito", category:"Food", amount: 8.75},
+  {date: "2019-12-06", description:"Chipotle", category:"Food", amount: 25 },
+]
 
 function App() {
+  return(
 
-
-  <>
-  <h1>The Royal Bank of Flatiron</h1>
-  </>
+  <div>
+ <TransactionList transactions ={fetchedTransactions} />
+  </div>
+  )
 }
 
 export default App;
